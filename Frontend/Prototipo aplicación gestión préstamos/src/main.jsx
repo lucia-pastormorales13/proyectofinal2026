c11e18be6a78f45b6e19367f7e324b0b9e9fb315
lucia <luciapasmoriesberenguer@gmail.com>
2026-06-02 20:57:22 +0200

    he quitado el frontend que hice con react y he configurado los archivos de config de docker y nginx

--- Frontend/Prototipo aplicación gestión préstamos/src/main.jsx
+++
@@ -1,6 +0,0 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx"; // 👈 Asegúrate de que apunte a tu nuevo App.jsx
-import "./styles/index.css";
-
-// Quitamos el "!" del getElementById porque en JavaScript no se usa
-createRoot(document.getElementById("root")).render(<App />);
